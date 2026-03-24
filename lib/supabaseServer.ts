ddd67d04ab5d371779dcc835eaf7ce33743877b4
chenying <yoyomibaobao@gamil.com>
2026-03-24 21:29:28 +0800

    fix discover link

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -4,13 +4,9 @@ import { cookies } from "next/headers";
 export async function getSupabaseServer() {
   const cookieStore = await cookies();
 
-  // ✅ 调试用（必须保留这行，先看结果）
-  console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-  console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    "https://eidltoikfpnrzxruvawf.supabase.co",
+    "sb_publishable_2cw-3WJnyD6x_CnPiDEiqQ_04violBa",
     {
       cookies: {
         get(name: string) {
