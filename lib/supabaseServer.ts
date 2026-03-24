6ea233de8b42f7464d14e6288614241f8fc5963e
chenying <yoyomibaobao@gamil.com>
2026-03-24 19:08:37 +0800

    fix: file type

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,6 +1,20 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export function getSupabaseServer() {
-  return createServerComponentClient({ cookies });
+  const cookieStore = cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set() {},
+        remove() {},
+      },
+    }
+  );
 }
