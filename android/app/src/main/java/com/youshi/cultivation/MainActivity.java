63b76ad3db5a3dd1f57f3abde8c607cfc838c979
yoyomibaobao-sys <yoyomibaobao@gmail.com>
2026-09-24 11:17:22 +0800

    Enable temporary WebView inspection for Android shell diagnostics

--- android/app/src/main/java/com/youshi/cultivation/MainActivity.java
+++ android/app/src/main/java/com/youshi/cultivation/MainActivity.java
@@ -1,18 +1,24 @@
 package com.youshi.cultivation;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        WebView.setWebContentsDebuggingEnabled(true);
         registerPlugin(NativeAppUpdatePlugin.class);
         registerPlugin(NativeSystemUiPlugin.class);
         super.onCreate(savedInstanceState);
         if (bridge != null) {
             bridge.getWebView().setWebViewClient(
                 new LifeSpaceWebViewClient(bridge, getAssets())
+            );
+            android.util.Log.i(
+                "LifeSpaceShell",
+                "WebView debugging enabled; initial URL: " + bridge.getWebView().getUrl()
             );
         }
     }
